df0d352f365e9182bb6211fcc9fd54f768bd9f71
AndyBeable <andy.beable@gmail.com>
2021-10-11 10:42:52 +0100

    countryitem comp

---
+++ src/components/CountryItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="country-wrapper">
+    <div>
+      <img src="" alt="" />
+    </div>
+    <div>
+      <h2>{{ name }}</h2>
+      <p>{{ population }}</p>
+      <p>{{ region }}</p>
+      <p>{{ capital }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["flag", "name", "population", "region", "capital"],
+};
+</script>
--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -1,14 +1,28 @@
 <template>
   <ul>
-    <li v-for="country in countries" :key="country.name">{{ country.name }}</li>
+    <li
+      v-for="country in countries"
+      :key="country.name"
+      :flag="country.flag"
+      :name="country.name"
+      :population="country.population"
+      :region="country.region"
+      :capital="country.capital"
+    >
+      <CountryItem />
+    </li>
   </ul>
 </template>
 
 <script>
 import Api from "../api/countries";
+import CountryItem from "../components/CountryItem.vue";
 
 export default {
   name: "CountryList",
+  components: {
+    CountryItem,
+  },
 
   data() {
     return {
@@ -19,6 +33,7 @@ export default {
   mounted() {
     Api.fetch().then(({ data }) => {
       this.countries = data;
+      console.log(data);
     });
   },
 };
