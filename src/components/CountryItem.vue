e6e68974a624effea6a22240872ca02aa558eff2
AndyBeable <andy.beable@gmail.com>
2021-10-11 12:22:20 +0100

    country cards height

--- src/components/CountryItem.vue
+++ src/components/CountryItem.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="country-card">
+  <div class="country_card">
     <a href="#">
-      <div>
+      <div class="country_card-img">
         <img :src="flag" alt="" />
       </div>
-      <div class="country-stats">
+      <div class="country_stats">
         <h2>{{ name }}</h2>
-        <p>{{ population }}</p>
-        <p>{{ region }}</p>
-        <p>{{ capital }}</p>
+        <p>
+          <span>Popluation:</span>
+          {{ population }}
+        </p>
+        <p>
+          <span>Region:</span>
+          {{ region }}
+        </p>
+        <p>
+          <span>Capital:</span>
+          {{ capital }}
+        </p>
       </div>
     </a>
   </div>
@@ -20,21 +29,43 @@ export default {
 };
 </script>
 
-<style scoped>
-.country-card {
+<style lang="scss" scoped>
+.country_card {
   display: flex;
   margin-bottom: 5rem;
   box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.1);
-  border-radius: 15px;
+  border-radius: 8px;
+  width: 20rem;
+  height: 25rem;
+
+  &-img {
+    max-width: 100%;
+  }
+
+  & img {
+    /* width: 100%; */
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+  }
 }
 
-.country-stats {
+.country_stats {
   display: flex;
   flex-direction: column;
   justify-content: center;
+  padding: 1rem 2rem;
+  width: 95%;
+
+  & h2 {
+    margin-bottom: 1rem;
+  }
 }
 
-img {
-  width: 100%;
+p {
+  margin: 3px 0;
+}
+
+p span {
+  font-weight: 800;
 }
 </style>
